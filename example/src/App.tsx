316e7d77837d6caaed3e9fc9863a6d0ee9707a34
Herman Khodyrev <german.khodyrev@banuba.com>
2026-05-11 18:18:53 +0300

    update main screen ui

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity, Platform } from 'react-native';
+import { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import VideoEditorPlugin, {
-  AiClipping,
-  Captions,
-  AudioBrowser,
-  AudioBrowserSource,
-  DraftsConfig,
-  DraftsOption,
-  EditorConfig,
-  GifPickerConfig,
   FeaturesConfigBuilder,
-  ExportData,
-  ExportedVideo,
-  Watermark,
-  VideoResolution,
-  WatermarkAlignment,
-  VideoDurationConfig,
+  TemplatesConfig,
 } from 'video-editor-react-native';
 
-import { launchImageLibrary } from 'react-native-image-picker';
+import {
+  launchImageLibrary,
+  type ImageLibraryOptions,
+} from 'react-native-image-picker';
 
 const LICENSE_TOKEN = SET BANUBA LICENSE TOKEN
 
-const videoOptions = { mediaType: 'video' };
+const videoOptions: ImageLibraryOptions = { mediaType: 'video' };
 
-export default class App extends Component {
+type AppState = {
+  errorText: string;
+};
 
+export default class App extends Component<Record<string, never>, AppState> {
   private featuresConfig = new FeaturesConfigBuilder()
     // Specify your Config params in the builder below
     //.setAudioBrowser(...)
     //...
+    .build();
+
+  private templateBuilderFeaturesConfig = new FeaturesConfigBuilder()
+    .setTemplatesConfig(
+      new TemplatesConfig({
+        url: null,
+        enableBuilder: true,
+        termsOfUseUrl: 'https://www.banuba.com/terms',
+      })
+    )
     .build();
 
   //   Export Data example
@@ -52,52 +62,69 @@ export default class App extends Component {
   //   }),
   // });
 
-  constructor() {
-    super();
+  constructor(props: Record<string, never>) {
+    super(props);
     this.state = {
       errorText: '',
     };
   }
 
-  handleVideoExport(response) {
-    let exportedVideoSources = response?.exportedVideoSources
-    let exportedPreview = response?.exportedPreview
-    let exportedMeta = response?.exportedMeta
-    let exportedDraftId = response?.savedDraftId
-    console.log('Export completed successfully: video = ' + exportedVideoSources + '; videoPreview = '
-      + exportedPreview + "; meta = " + exportedMeta + ": savedDraftId = " + exportedDraftId);
+  handleVideoExport(response: any) {
+    let exportedVideoSources = response?.exportedVideoSources;
+    let exportedPreview = response?.exportedPreview;
+    let exportedMeta = response?.exportedMeta;
+    let exportedDraftId = response?.savedDraftId;
+    console.log(
+      'Export completed successfully: video = ' +
+        exportedVideoSources +
+        '; videoPreview = ' +
+        exportedPreview +
+        '; meta = ' +
+        exportedMeta +
+        ': savedDraftId = ' +
+        exportedDraftId
+    );
   }
 
-  handleSdkError(e) {
+  handleSdkError(e: { code?: string; message?: string }) {
     console.log('handle sdk error = ' + e.code);
 
     var message = '';
     switch (e.code) {
       case 'ERR_SDK_NOT_INITIALIZED':
-        message = 'Failed to initialize SDK!!! The license token is incorrect: empty or truncated. Please check the license token and try again.';
+        message =
+          'Failed to initialize SDK!!! The license token is incorrect: empty or truncated. Please check the license token and try again.';
         break;
       case 'ERR_SDK_LICENSE_REVOKED':
-        message = 'WARNING!!! YOUR LICENSE TOKEN EITHER EXPIRED OR REVOKED! Please contact Banuba';
+        message =
+          'WARNING!!! YOUR LICENSE TOKEN EITHER EXPIRED OR REVOKED! Please contact Banuba';
         break;
       case 'ERR_MISSING_EXPORT_RESULT':
-        message = 'Missing export result: video export has not been completed successfully. Please try again';
+        message =
+          'Missing export result: video export has not been completed successfully. Please try again';
+        break;
       case 'ERR_MISSING_HOST':
-        message = "Missing host Activity to start video editor";
+        message = 'Missing host Activity to start video editor';
+        break;
       case 'ERR_MISSING_DRAFT_ID':
         message = "Provided Draft ID doesn't exist!";
+        break;
       case 'ERR_VIDEO_EXPORT_CANCEL':
-        message = "The user has canceled video editing flow!";
+        message = 'The user has canceled video editing flow!';
+        break;
       case 'ERR_ENTRY_NOT_SUPPORTED':
-        message = "Draft by ID is not supported on Android"
+        message = 'Draft by ID is not supported on Android';
+        break;
       case 'ERR_INVALID_PARAMS':
-        message = e.message;
+        message = e.message ?? '';
+        break;
       default:
         message = '';
         console.log(
           'Banuba ' +
-          Platform.OS.toUpperCase() +
-          ' Video Editor export failed = ' +
-          e,
+            Platform.OS.toUpperCase() +
+            ' Video Editor export failed = ' +
+            e
         );
         break;
     }
@@ -107,24 +134,27 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.title}>
-            Sample integration of Banuba Video into React Native project
+        <SafeAreaView style={styles.appBar}>
+          <Text style={styles.appBarTitle}>
+            Video Editor React Native plugin
           </Text>
-        </View>
-        <View style={styles.buttonsWrapper}>
-          <View style={styles.buttonsContainer}>
+        </SafeAreaView>
+        <View style={styles.content}>
+          <View style={styles.errorContainer}>
             {this.state.errorText ? (
               <Text style={styles.errorText}>{this.state.errorText}</Text>
             ) : null}
+          </View>
 
+          <View style={styles.buttonsContainer}>
             <TouchableOpacity
               style={styles.button}
               onPress={async () => {
                 const videoEditor = new VideoEditorPlugin();
-                videoEditor.openFromCamera(LICENSE_TOKEN, this.featuresConfig)
-                  .then(response => this.handleVideoExport(response))
-                  .catch(e => this.handleSdkError(e));
+                videoEditor
+                  .openFromCamera(LICENSE_TOKEN, this.featuresConfig)
+                  .then((response) => this.handleVideoExport(response))
+                  .catch((e) => this.handleSdkError(e));
               }}
             >
               <Text style={styles.buttonText}>Open Video Editor - Default</Text>
@@ -133,25 +163,66 @@ export default class App extends Component {
             <TouchableOpacity
               style={styles.button}
               onPress={async () => {
-                launchImageLibrary(
-                  videoOptions,
-                  (response) => {
-                    console.log('Response = ', response);
-                    if (response.didCancel) {
-                      console.warn('User cancelled photo picker');
-                    } else if (response.error) {
-                      console.warn('ImagePicker Error: ', response.error);
-                    } else {
-                      let videoUri = response.uri || response.assets?.[0]?.uri;
-                      console.log('# Picked video source for pip = ' + videoUri);
+                const videoEditor = new VideoEditorPlugin();
+                videoEditor
+                  .openFromTemplates(LICENSE_TOKEN, this.featuresConfig)
+                  .then((response) => this.handleVideoExport(response))
+                  .catch((e) => this.handleSdkError(e));
+              }}
+            >
+              <Text style={styles.buttonText}>
+                Open Video Editor - Templates
+              </Text>
+            </TouchableOpacity>
 
-                      const videoEditor = new VideoEditorPlugin();
-                      videoEditor.openFromPip(LICENSE_TOKEN, this.featuresConfig, videoUri)
-                        .then(response => { this.handleVideoExport(response); })
-                        .catch(e => { this.handleSdkError(e); });
+            <TouchableOpacity
+              style={styles.button}
+              onPress={async () => {
+                const videoEditor = new VideoEditorPlugin();
+                videoEditor
+                  .openFromTemplates(
+                    LICENSE_TOKEN,
+                    this.templateBuilderFeaturesConfig
+                  )
+                  .then((response) => this.handleVideoExport(response))
+                  .catch((e) => this.handleSdkError(e));
+              }}
+            >
+              <Text style={styles.buttonText}>
+                Open Video Editor - Templates Builder
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={async () => {
+                launchImageLibrary(videoOptions, (response) => {
+                  console.log('Response = ', response);
+                  if (response.didCancel) {
+                    console.warn('User cancelled photo picker');
+                  } else if (response.errorCode) {
+                    console.warn('ImagePicker Error: ', response.errorMessage);
+                  } else {
+                    let videoUri = response.assets?.[0]?.uri;
+                    if (!videoUri) {
+                      console.warn(
+                        'Cannot start video editor in pip mode: please pick video file'
+                      );
+                      return;
                     }
-                  },
-                );
+                    console.log('# Picked video source for pip = ' + videoUri);
+
+                    const videoEditor = new VideoEditorPlugin();
+                    videoEditor
+                      .openFromPip(LICENSE_TOKEN, this.featuresConfig, videoUri)
+                      .then((exportResponse) => {
+                        this.handleVideoExport(exportResponse);
+                      })
+                      .catch((e) => {
+                        this.handleSdkError(e);
+                      });
+                  }
+                });
               }}
             >
               <Text style={styles.buttonText}>Open Video Editor - PIP</Text>
@@ -160,26 +231,40 @@ export default class App extends Component {
             <TouchableOpacity
               style={styles.button}
               onPress={async () => {
-                launchImageLibrary(
-                  videoOptions,
-                  (response) => {
-                    console.log('Response = ', response);
-                    if (response.didCancel) {
-                      console.warn('User cancelled photo picker');
-                    } else if (response.error) {
-                      console.warn('ImagePicker Error: ', response.error);
-                    } else {
-                      let videoUri = response.uri || response.assets?.[0]?.uri;
-                      console.log('# Picked video source for trimmer = ' + videoUri);
-                      let videoSources = [videoUri];
-
-                      const videoEditor = new VideoEditorPlugin();
-                      videoEditor.openFromTrimmer(LICENSE_TOKEN, this.featuresConfig, videoSources)
-                        .then(response => { this.handleVideoExport(response); })
-                        .catch(e => { this.handleSdkError(e); });
+                launchImageLibrary(videoOptions, (response) => {
+                  console.log('Response = ', response);
+                  if (response.didCancel) {
+                    console.warn('User cancelled photo picker');
+                  } else if (response.errorCode) {
+                    console.warn('ImagePicker Error: ', response.errorMessage);
+                  } else {
+                    let videoUri = response.assets?.[0]?.uri;
+                    if (!videoUri) {
+                      console.warn(
+                        'Cannot start video editor in trimmer mode: please pick video file'
+                      );
+                      return;
                     }
-                  },
-                );
+                    console.log(
+                      '# Picked video source for trimmer = ' + videoUri
+                    );
+                    let videoSources = [videoUri];
+
+                    const videoEditor = new VideoEditorPlugin();
+                    videoEditor
+                      .openFromTrimmer(
+                        LICENSE_TOKEN,
+                        this.featuresConfig,
+                        videoSources
+                      )
+                      .then((exportResponse) => {
+                        this.handleVideoExport(exportResponse);
+                      })
+                      .catch((e) => {
+                        this.handleSdkError(e);
+                      });
+                  }
+                });
               }}
             >
               <Text style={styles.buttonText}>Open Video Editor - Trimmer</Text>
@@ -196,25 +281,31 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex: 1,
   },
-  headerContainer: {
-    height: '33%',
+  appBar: {
+    backgroundColor: 'black',
+    minHeight: 56,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 16,
   },
-  title: {
+  appBarTitle: {
+    color: 'white',
     fontSize: 18,
+    fontWeight: '600',
     textAlign: 'center',
   },
-  buttonsWrapper: {
-    position: 'absolute',
-    top: 50,
-    bottom: 0,
-    left: 0,
-    right: 0,
+  content: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 16,
+  },
+  errorContainer: {
+    position: 'absolute',
+    top: 8,
+    left: 16,
+    right: 16,
+    alignItems: 'center',
   },
   buttonsContainer: {
     alignItems: 'center',
@@ -223,27 +314,27 @@ const styles = StyleSheet.create({
   },
   errorText: {
     color: '#ff0000',
-    fontSize: 16,
-    fontWeight: '800',
+    fontSize: 17,
     textAlign: 'center',
-    marginBottom: 16,
   },
   button: {
-    backgroundColor: '#007bff',
-    paddingVertical: 12,
+    backgroundColor: 'black',
+    height: 50,
     borderRadius: 30,
     alignItems: 'center',
+    justifyContent: 'center',
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5,
+    shadowOffset: { width: 0, height: 6 },
+    shadowOpacity: 0.35,
+    shadowRadius: 8,
+    elevation: 10,
     width: '100%',
-    marginVertical: 8,
+    marginVertical: 10,
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 13,
     fontWeight: '600',
+    textAlign: 'center',
   },
 });
